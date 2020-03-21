ef62cae56d765e579130278559de775b87c81671
JiaoZhuangFei <18035877@suning.com>
2020-03-21 18:30:48 +0800

    init

---
+++ src/components/SecondPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="second-page">
+    second page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SecondPage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  .second-page {
+    background-color: red;
+    height: 100px;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import SecondPage from '@/components/SecondPage'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/second',
+      name: 'SecondPage',
+      component: SecondPage,
+      beforeEnter: (to, from, next) => {
+        console.log('secode beforeEnter', to)
+        console.log(from, next)
+        next()
+      }
+    }
+  ]
+})
